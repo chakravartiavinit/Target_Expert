44b60bde32e122586670e8cfcb74e25c416dae34
Chakravarti Avinit <chakravartiavinit@Chakravartis-MacBook-Air.local>
2022-12-23 17:53:30 +0530

    Update

--- Avinit/C_Virus.cpp
+++ Avinit/C_Virus.cpp
@@ -16,22 +16,13 @@
 #include <iomanip>
 using namespace std;
 
-// LLLLLLLLLLL                               lllllll
-// L:::::::::L                               l:::::l
-// L:::::::::L                               l:::::l
-// LL:::::::LL                               l:::::l
-//   L:::::L               uuuuuu    uuuuuu   l::::l zzzzzzzzzzzzzzzzz    ssssssssss       eeeeeeeeeeee        cccccccccccccccc
-//   L:::::L               u::::u    u::::u   l::::l z:::::::::::::::z  ss::::::::::s    ee::::::::::::ee    cc:::::::::::::::c
-//   L:::::L               u::::u    u::::u   l::::l z::::::::::::::z ss:::::::::::::s  e::::::eeeee:::::ee c:::::::::::::::::c
-//   L:::::L               u::::u    u::::u   l::::l zzzzzzzz::::::z  s::::::ssss:::::se::::::e     e:::::ec:::::::cccccc:::::c
-//   L:::::L               u::::u    u::::u   l::::l       z::::::z    s:::::s  ssssss e:::::::eeeee::::::ec::::::c     ccccccc
-//   L:::::L               u::::u    u::::u   l::::l      z::::::z       s::::::s      e:::::::::::::::::e c:::::c
-//   L:::::L               u::::u    u::::u   l::::l     z::::::z           s::::::s   e::::::eeeeeeeeeee  c:::::c
-//   L:::::L         LLLLLLu:::::uuuu:::::u   l::::l    z::::::z      ssssss   s:::::s e:::::::e           c::::::c     ccccccc
-// LL:::::::LLLLLLLLL:::::Lu:::::::::::::::uul::::::l  z::::::zzzzzzzzs:::::ssss::::::se::::::::e          c:::::::cccccc:::::c
-// L::::::::::::::::::::::L u:::::::::::::::ul::::::l z::::::::::::::zs::::::::::::::s  e::::::::eeeeeeee   c:::::::::::::::::c
-// L::::::::::::::::::::::L  uu::::::::uu:::ul::::::lz:::::::::::::::z s:::::::::::ss    ee:::::::::::::e    cc:::::::::::::::c
-// LLLLLLLLLLLLLLLLLLLLLLLL    uuuuuuuu  uuuullllllllzzzzzzzzzzzzzzzzz  sssssssssss        eeeeeeeeeeeeee      cccccccccccccccc
+//  ████████  ██████      ████████ ██   ██ ███████     ████████  ██████  ██████
+//     ██    ██    ██        ██    ██   ██ ██             ██    ██    ██ ██   ██
+//     ██    ██    ██        ██    ███████ █████          ██    ██    ██ ██████
+//     ██    ██    ██        ██    ██   ██ ██             ██    ██    ██ ██
+//     ██     ██████         ██    ██   ██ ███████        ██     ██████  ██
+//
+//
 #define MOD 1000000007
 #define PI 3.141592653589793238462
 #define int long long
@@ -127,55 +118,46 @@ signed main()
     cin.tie(NULL);
     cout.tie(NULL);
     int t;
+    cin >> t;
     while (t--)
     {
         int n, m;
         cin >> n >> m;
-        vector<int> infected(m);
-        set<int> all_infected, current_infected;
+        vector<int> infected_houses(m);
         for (int i = 0; i < m; ++i)
         {
-            cin >> infected[i];
-            all_infected.insert(infected[i]);
-            current_infected.insert(infected[i]);
+            cin >> infected_houses[i];
         }
-        while (true)
+        sort(infected_houses.begin(), infected_houses.end());
+        vector<int> segments;
+        for (int i = 0; i + 1 < m; ++i)
         {
-            int current_protected_house = -1;
-            for (auto x : current_infected)
+            segments.push_back(infected_houses[i + 1] - infected_houses[i] - 1);
+        }
+        segments.push_back(infected_houses[0] - infected_houses[m - 1] + n - 1);
+        sort(segments.begin(), segments.end(), greater<int>());
+        int protected_houses = 0, days = 0;
+        for (int i = 0; i < segments.size(); ++i)
+        {
+            int alpha = segments[i];
+            alpha -= 2 * days;
+            if (alpha > 0)
             {
-                if ((x - 1) >= 1 && all_infected.find(x - 1) == all_infected.end())
+                if (alpha == 1)
                 {
-                    current_protected_house = x - 1;
-                    break;
+                    protected_houses++;
                 }
-                if ((x + 1) <= n && all_infected.find(x + 1) == all_infected.end())
+                else
                 {
-                    current_protected_house = x + 1;
-                    break;
+                    protected_houses += (alpha - 1);
                 }
             }
-            debug(current_protected_house);
-            if (current_protected_house == -1)
+            else
             {
                 break;
             }
-            for (auto x : current_infected)
-            {
-                int current_house = x;
-                if ((current_house - 1) >= 0 && current_house - 1 != current_protected_house)
-                {
-                    current_infected.insert(current_house - 1);
-                    all_infected.insert(current_house - 1);
-                }
-                if ((current_house + 1) >= 0 && current_house + 1 != current_protected_house)
-                {
-                    current_infected.insert(current_house + 1);
-                    all_infected.insert(current_house + 1);
-                }
-                // current_infected.erase(current_house);
-            }
-            cout << all_infected.size() << "\n";
+            days += 2;
         }
+        cout << n - protected_houses << "\n";
     }
 }
