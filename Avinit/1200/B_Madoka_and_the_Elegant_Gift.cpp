964ca002548ac9aa63d90af1dc6e185a9a55ea1e
Chakravarti Avinit <chakravartiavinit@Chakravartis-MacBook-Air.local>
2022-12-27 16:30:53 +0530

    Added Solution

---
+++ Avinit/1200/B_Madoka_and_the_Elegant_Gift.cpp
@@ -0,0 +1,165 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <list>
+#include <set>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <cstring>
+#include <cmath>
+#include <bitset>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstdlib>
+#include <iomanip>
+using namespace std;
+
+//  ████████  ██████      ████████ ██   ██ ███████     ████████  ██████  ██████
+//     ██    ██    ██        ██    ██   ██ ██             ██    ██    ██ ██   ██
+//     ██    ██    ██        ██    ███████ █████          ██    ██    ██ ██████
+//     ██    ██    ██        ██    ██   ██ ██             ██    ██    ██ ██
+//     ██     ██████         ██    ██   ██ ███████        ██     ██████  ██
+//
+//
+#define MOD 1000000007
+#define PI 3.141592653589793238462
+#define int long long
+
+/*########################################### DEBUG ###############################################################################################*/
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t)
+{
+    cerr << t;
+}
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(long double t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(unsigned long long t) { cerr << t; }
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+    cerr << "{";
+    _print(p.first);
+    cerr << ",";
+    _print(p.second);
+    cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+/*###############################################################################################################################################*/
+bool check(int i, int j, int n, int m)
+{
+    if (i < 0 || j < 0 || i >= n || j >= m)
+    {
+        return false;
+    }
+    return true;
+}
+bool Intersect(vector<string> &vt, int n, int m)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            if (check(i + 1, j, n, m) == true && check(i, j + 1, n, m) == true && check(i + 1, j + 1, n, m) == true)
+            {
+                if ((vt[i][j] - '0') + (vt[i + 1][j] - '0') + (vt[i][j + 1] - '0') + (vt[i + 1][j + 1] - '0') == 3)
+                {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+        vector<string> vt(n);
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> vt[i];
+        }
+        if (Intersect(vt, n, m) == true)
+        {
+            cout << "NO\n";
+        }
+        else
+        {
+            cout << "YES\n";
+        }
+    }
+}
