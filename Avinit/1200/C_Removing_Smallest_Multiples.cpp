9097e3d2299e00bd185062b19e59d955c185da99
Chakravarti Avinit <chakravartiavinit@Chakravartis-MacBook-Air.local>
2023-01-02 01:04:21 +0530

    Solution of Removing Smallest Multiples

---
+++ Avinit/1200/C_Removing_Smallest_Multiples.cpp
@@ -0,0 +1,172 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <list>
+#include <set>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <cstring>
+#include <cmath>
+#include <bitset>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstdlib>
+#include <iomanip>
+using namespace std;
+
+//  ████████  ██████      ████████ ██   ██ ███████     ████████  ██████  ██████
+//     ██    ██    ██        ██    ██   ██ ██             ██    ██    ██ ██   ██
+//     ██    ██    ██        ██    ███████ █████          ██    ██    ██ ██████
+//     ██    ██    ██        ██    ██   ██ ██             ██    ██    ██ ██
+//     ██     ██████         ██    ██   ██ ███████        ██     ██████  ██
+//
+//
+#define MOD 1000000007
+#define PI 3.141592653589793238462
+#define int long long
+
+/*########################################### DEBUG ###############################################################################################*/
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t)
+{
+    cerr << t;
+}
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(long double t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(unsigned long long t) { cerr << t; }
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+    cerr << "{";
+    _print(p.first);
+    cerr << ",";
+    _print(p.second);
+    cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+/*###############################################################################################################################################*/
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        string alpha;
+        cin >> alpha;
+        unordered_set<int> st, st1;
+        for (int i = 0; i < alpha.size(); ++i)
+        {
+            if (alpha[i] == '0')
+            {
+                st.insert(i + 1);
+            }
+            else
+            {
+                st1.insert(i + 1);
+            }
+        }
+        int answer = 0;
+        for (int i = 1; i <= n; ++i)
+        {
+            bool flag = false;
+            for (int j = i; j <= n; j += i)
+            {
+                if (st.size() == 0)
+                {
+                    flag = true;
+                    break;
+                }
+                if (st1.find(j) != st1.end())
+                {
+                    break;
+                }
+                else
+                {
+                    if (st.find(j) != st.end())
+                    {
+                        answer += i;
+                        st.erase(j);
+                    }
+                }
+            }
+            if (flag == true)
+            {
+                break;
+            }
+        }
+        cout << answer << "\n";
+    }
+}
