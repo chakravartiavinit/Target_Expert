02632b71468346e06c49c99e8828fac25ea2ed5e
Chakravarti Avinit <chakravartiavinit@Chakravartis-MacBook-Air.local>
2023-01-01 19:41:25 +0530

    Solution for Weight of the System of Nested Segments

---
+++ Avinit/1200/C_Weight_of_the_System_of_Nested_Segments.cpp
@@ -0,0 +1,155 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <list>
+#include <set>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <cstring>
+#include <cmath>
+#include <bitset>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstdlib>
+#include <iomanip>
+using namespace std;
+
+//  ████████  ██████      ████████ ██   ██ ███████     ████████  ██████  ██████
+//     ██    ██    ██        ██    ██   ██ ██             ██    ██    ██ ██   ██
+//     ██    ██    ██        ██    ███████ █████          ██    ██    ██ ██████
+//     ██    ██    ██        ██    ██   ██ ██             ██    ██    ██ ██
+//     ██     ██████         ██    ██   ██ ███████        ██     ██████  ██
+//
+//
+#define MOD 1000000007
+#define PI 3.141592653589793238462
+#define int long long
+
+/*########################################### DEBUG ###############################################################################################*/
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t)
+{
+    cerr << t;
+}
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(long double t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(unsigned long long t) { cerr << t; }
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+    cerr << "{";
+    _print(p.first);
+    cerr << ",";
+    _print(p.second);
+    cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+/*###############################################################################################################################################*/
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+        vector<int> weight(m), coordinate(m);
+        vector<pair<int, int>> vp;
+        map<int, int> mp;
+        for (int i = 0; i < m; ++i)
+        {
+            cin >> coordinate[i];
+            cin >> weight[i];
+            mp[coordinate[i]] = i + 1;
+            vp.push_back({weight[i], coordinate[i]});
+        }
+        int total_weight = 0;
+        vector<int> temp;
+        sort(vp.begin(),vp.end());
+        for (int i = 0; i < 2 * n; ++i)
+        {
+            total_weight += vp[i].first;
+            temp.push_back(vp[i].second);
+        }
+        sort(temp.begin(), temp.end());
+        cout << total_weight << "\n";
+        int i = 0, j = temp.size() - 1;
+        while (i < j)
+        {
+            cout << mp[temp[i]] << " " << mp[temp[j]] << "\n";
+            i++;
+            j--;
+        }
+    }
+}
