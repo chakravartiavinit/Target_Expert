ee17cfce0639af6eb2008d0a68cd3a68cff599d4
Chakravarti Avinit <chakravartiavinit@Chakravartis-MacBook-Air.local>
2023-01-06 14:57:15 +0530

    Solution of Hemose Shopping

---
+++ Avinit/1200/B_Hemose_Shopping.cpp
@@ -0,0 +1,156 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <list>
+#include <set>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <cstring>
+#include <cmath>
+#include <bitset>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstdlib>
+#include <iomanip>
+using namespace std;
+
+//  ████████  ██████      ████████ ██   ██ ███████     ████████  ██████  ██████
+//     ██    ██    ██        ██    ██   ██ ██             ██    ██    ██ ██   ██
+//     ██    ██    ██        ██    ███████ █████          ██    ██    ██ ██████
+//     ██    ██    ██        ██    ██   ██ ██             ██    ██    ██ ██
+//     ██     ██████         ██    ██   ██ ███████        ██     ██████  ██
+//
+//
+#define MOD 1000000007
+#define PI 3.141592653589793238462
+#define int long long
+
+/*########################################### DEBUG ###############################################################################################*/
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t)
+{
+    cerr << t;
+}
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(long double t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(unsigned long long t) { cerr << t; }
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+    cerr << "{";
+    _print(p.first);
+    cerr << ",";
+    _print(p.second);
+    cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+/*###############################################################################################################################################*/
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        vector<int> vt(n);
+        map<int, set<int, greater<int>>> mp;
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> vt[i];
+            mp[vt[i]].insert(i);
+        }
+        vector<int> sorted_vt = vt;
+        sort(sorted_vt.begin(), sorted_vt.end());
+        if (sorted_vt == vt)
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            bool possible = true;
+            int start = n - k;
+            int end = k - 1;
+            for (int i = start; i <= end; ++i)
+            {
+                if (sorted_vt[i] != vt[i])
+                {
+                    possible = false;
+                    break;
+                }
+            }
+            possible == true ? cout << "YES\n" : cout << "NO\n";
+        }
+    }
+}
