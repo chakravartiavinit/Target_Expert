02cebed958b4a270f144da92ef552505e1034186
Chakravarti Avinit <chakravartiavinit@Chakravartis-MacBook-Air.local>
2023-01-07 04:30:07 +0530

    Solution of Pursuit

---
+++ Avinit/1200/C_Pursuit.cpp
@@ -0,0 +1,186 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <list>
+#include <set>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <cstring>
+#include <cmath>
+#include <bitset>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstdlib>
+#include <iomanip>
+using namespace std;
+
+//  ████████  ██████      ████████ ██   ██ ███████     ████████  ██████  ██████
+//     ██    ██    ██        ██    ██   ██ ██             ██    ██    ██ ██   ██
+//     ██    ██    ██        ██    ███████ █████          ██    ██    ██ ██████
+//     ██    ██    ██        ██    ██   ██ ██             ██    ██    ██ ██
+//     ██     ██████         ██    ██   ██ ███████        ██     ██████  ██
+//
+//
+#define MOD 1000000007
+#define PI 3.141592653589793238462
+#define int long long
+
+/*########################################### DEBUG ###############################################################################################*/
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t)
+{
+    cerr << t;
+}
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(long double t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(unsigned long long t) { cerr << t; }
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+    cerr << "{";
+    _print(p.first);
+    cerr << ",";
+    _print(p.second);
+    cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+/*###############################################################################################################################################*/
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n), b(n);
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> a[i];
+        }
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> b[i];
+        }
+        sort(a.rbegin(), a.rend());
+        sort(b.rbegin(), b.rend());
+        int k = n - ((n) / 4);
+        int sum_a = 0, sum_b = 0;
+        for (int i = 0; i < k; ++i)
+        {
+            sum_a += a[i];
+            sum_b += b[i];
+        }
+        if (sum_a >= sum_b)
+        {
+            cout << "0\n";
+        }
+        else
+        {
+            reverse(a.begin(), a.end());
+            reverse(b.begin(), b.end());
+            int answer = 1;
+            vector<int> preffix, preffix1;
+            preffix.push_back(a[0]);
+            preffix1.push_back(b[0]);
+            for (int i = 1; i < n; ++i)
+            {
+                preffix.push_back(preffix.back() + a[i]);
+                preffix1.push_back(preffix1.back() + b[i]);
+            }
+            while (true)
+            {
+                preffix.push_back(preffix.back() + 100);
+                int rounds = n + answer;
+                int size = rounds - ((rounds) / 4);
+                int sum1 = preffix.back();
+                int sum2 = preffix1.back();
+                if (rounds > size)
+                {
+                    sum1 -= preffix[rounds - size - 1];
+                }
+                if (size < n)
+                {
+                    sum2 -= preffix1[n - size - 1];
+                }
+                if (sum1 >= sum2)
+                {
+                    break;
+                }
+                answer++;
+            }
+            cout << answer << "\n";
+        }
+    }
+}
