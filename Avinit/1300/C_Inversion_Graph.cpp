170fdcaa91a7ea5ec28ad4257fae60d69b426d65
Chakravarti Avinit <chakravartiavinit@Chakravartis-MacBook-Air.local>
2023-01-13 11:24:35 +0530

    Solution of Inversion Graph

---
+++ Avinit/1300/C_Inversion_Graph.cpp
@@ -0,0 +1,181 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <list>
+#include <set>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <cstring>
+#include <cmath>
+#include <bitset>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstdlib>
+#include <iomanip>
+using namespace std;
+
+//  ████████  ██████      ████████ ██   ██ ███████     ████████  ██████  ██████
+//     ██    ██    ██        ██    ██   ██ ██             ██    ██    ██ ██   ██
+//     ██    ██    ██        ██    ███████ █████          ██    ██    ██ ██████
+//     ██    ██    ██        ██    ██   ██ ██             ██    ██    ██ ██
+//     ██     ██████         ██    ██   ██ ███████        ██     ██████  ██
+//
+//
+#define MOD 1000000007
+#define PI 3.141592653589793238462
+#define int long long
+
+/*########################################### DEBUG ###############################################################################################*/
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t)
+{
+    cerr << t;
+}
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(long double t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(unsigned long long t) { cerr << t; }
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+    cerr << "{";
+    _print(p.first);
+    cerr << ",";
+    _print(p.second);
+    cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+/*###############################################################################################################################################*/
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> vt(n);
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> vt[i];
+        }
+        int Max = *max_element(vt.begin(), vt.end());
+        bool sortedData = true;
+        for (int i = 0; i + 1 < n; ++i)
+        {
+            if (vt[i] > vt[i + 1])
+            {
+                sortedData = false;
+                break;
+            }
+        }
+        if (sortedData == true)
+        {
+            cout << n << "\n";
+        }
+        else if (vt[0] == Max)
+        {
+            cout << "1\n";
+        }
+        else
+        {
+            map<int, int, greater<int>> mp;
+            for (int i = 0; i < n; ++i)
+            {
+                mp[vt[i]] = i;
+            }
+            vector<bool> visited(n + 1, false);
+            int Min = n + 10, answer = 0, end = n;
+            for (auto x : mp)
+            {
+                if (visited[x.first] == false)
+                {
+                    int number = x.first, index = x.second;
+                    if (Min == n + 10 || number < Min)
+                    {
+                        answer++;
+                    }
+                    int start = index;
+                    while (start < end)
+                    {
+                        Min = min(Min, vt[start]);
+                        visited[vt[start]] = true;
+                        start++;
+                    }
+                    end = index;
+                }
+            }
+            cout << answer << "\n";
+        }
+    }
+}
