e695ed0a19a65e533e936232e67a850e0040ae6b
Chakravarti Avinit <chakravartiavinit@Chakravartis-MacBook-Air.local>
2023-01-16 13:41:49 +0530

    Solution of AND 0 SUM BIG

---
+++ Avinit/1200/B_AND_0_Sum_Big.cpp
@@ -0,0 +1,159 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <list>
+#include <set>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <cstring>
+#include <cmath>
+#include <bitset>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstdlib>
+#include <iomanip>
+using namespace std;
+
+//  ████████  ██████      ████████ ██   ██ ███████     ████████  ██████  ██████
+//     ██    ██    ██        ██    ██   ██ ██             ██    ██    ██ ██   ██
+//     ██    ██    ██        ██    ███████ █████          ██    ██    ██ ██████
+//     ██    ██    ██        ██    ██   ██ ██             ██    ██    ██ ██
+//     ██     ██████         ██    ██   ██ ███████        ██     ██████  ██
+//
+//
+#define MOD 1000000007
+#define PI 3.141592653589793238462
+#define int long long
+
+/*########################################### DEBUG ###############################################################################################*/
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t)
+{
+    cerr << t;
+}
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(long double t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(unsigned long long t) { cerr << t; }
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+    cerr << "{";
+    _print(p.first);
+    cerr << ",";
+    _print(p.second);
+    cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+/*###############################################################################################################################################*/
+bool Check(vector<int> &vt)
+{
+    int start = vt[0];
+    for (int i = 1; i < vt.size(); ++i)
+    {
+        start = start & vt[i];
+    }
+    if (start == 0)
+    {
+        return true;
+    }
+    return false;
+}
+int res(int a,int b,int mod)
+{
+    a%=mod;
+    long long res=1;
+    while(b>0)
+    {
+        if(b&1)
+        {
+            res=(res*a)%mod;
+        }
+        a=a*a%mod;
+        b/=2;
+    }
+    return res%mod;
+}
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        int mod=1e9+7;
+        cout<<res(n,k,mod)<<"\n";
+       
+    }
+}
